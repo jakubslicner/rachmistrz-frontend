888328f52afbf2dc7dffee36b9f3595ed6e4a72d
jakubslicner <100052903+jakubslicner@users.noreply.github.com>
2025-10-02 21:53:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello from Rachmistrz Frontend!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
 
